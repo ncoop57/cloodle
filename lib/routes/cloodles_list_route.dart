798597e3e56efef3d601bfc116b54a869dfa46c2
ncoop57 <nac33@students.uwf.edu>
2018-07-17 00:58:59 -0500

    Removed duplicate code

--- lib/routes/cloodles_list_route.dart
+++ lib/routes/cloodles_list_route.dart
@@ -28,12 +28,12 @@ class CloodlesListRoute extends StatelessWidget {
           children: [
             new Column(
               children: <Widget>[
-                _displayCloodlesReceivedList(),
+                _displayCloodlesList("TO"),
               ],
             ),
             new Column(
               children: <Widget>[
-                _displayCloodlesSentList(),
+                _displayCloodlesList("FROM"),
               ],
             ),
           ],
@@ -42,41 +42,10 @@ class CloodlesListRoute extends StatelessWidget {
     );
   }
 
-  Widget _displayCloodlesReceivedList() {
+  Widget _displayCloodlesList(String condition) {
     var _db = Firestore.instance
         .collection("sessions")
-        .where("TO", isEqualTo: currentUser.notification_token);
-    return new StreamBuilder(
-      stream: _db.snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return const Text('Loading...');
-        final sessions = snapshot.data.documents;
-        return new Expanded(
-          child: ListView.builder(
-            shrinkWrap: true,
-            padding: EdgeInsets.all(8.0),
-            reverse: false,
-            itemBuilder: (_, int index) => new CloodleTile(
-                  session: new Session(
-                    session_id: sessions[index].documentID,
-                    from: sessions[index]['FROM'],
-                    from_name: sessions[index]['FROM_NAME'],
-                    to: sessions[index]['TO'],
-                    to_name: sessions[index]['TO_NAME'],
-                    image_name: sessions[index]['IMAGE_NAME'],
-                  ),
-                ),
-            itemCount: snapshot.data.documents.length,
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _displayCloodlesSentList() {
-    var _db = Firestore.instance
-        .collection("sessions")
-        .where("FROM", isEqualTo: currentUser.notification_token);
+        .where(condition, isEqualTo: currentUser.notification_token);
     return new StreamBuilder(
       stream: _db.snapshots(),
       builder: (context, snapshot) {
