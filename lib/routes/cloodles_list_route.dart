d6c8e77e4c9eec2e9781a6aa628a1e9639012841
ncoop57 <nac33@students.uwf.edu>
2018-07-17 00:52:31 -0500

    Turned cloodles list into tabbed view so user can view both cloodles sent to them and ones they sent

--- lib/routes/cloodles_list_route.dart
+++ lib/routes/cloodles_list_route.dart
@@ -12,22 +12,71 @@ class CloodlesListRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('Cloodles List'),
-      ),
-      body: new Column(
-        children: <Widget>[
-          _displayCloodlesList(),
-        ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.mail)),
+              Tab(icon: Icon(Icons.send)),
+            ],
+          ),
+          title: Text('Cloodles List'),
+        ),
+        body: TabBarView(
+          children: [
+            new Column(
+              children: <Widget>[
+                _displayCloodlesReceivedList(),
+              ],
+            ),
+            new Column(
+              children: <Widget>[
+                _displayCloodlesSentList(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _displayCloodlesList() {
+  Widget _displayCloodlesReceivedList() {
     var _db = Firestore.instance
         .collection("sessions")
         .where("TO", isEqualTo: currentUser.notification_token);
+    return new StreamBuilder(
+      stream: _db.snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return const Text('Loading...');
+        final sessions = snapshot.data.documents;
+        return new Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.all(8.0),
+            reverse: false,
+            itemBuilder: (_, int index) => new CloodleTile(
+                  session: new Session(
+                    session_id: sessions[index].documentID,
+                    from: sessions[index]['FROM'],
+                    from_name: sessions[index]['FROM_NAME'],
+                    to: sessions[index]['TO'],
+                    to_name: sessions[index]['TO_NAME'],
+                    image_name: sessions[index]['IMAGE_NAME'],
+                  ),
+                ),
+            itemCount: snapshot.data.documents.length,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _displayCloodlesSentList() {
+    var _db = Firestore.instance
+        .collection("sessions")
+        .where("FROM", isEqualTo: currentUser.notification_token);
     return new StreamBuilder(
       stream: _db.snapshots(),
       builder: (context, snapshot) {
